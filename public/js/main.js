a7f0babefdd1c50bcb12cd1d71e30acfab38566d
sahil singh <sahilsingh20698@gmail.com>
2022-12-02 18:14:01 +0530

    nodejs project my first commit

---
+++ public/js/main.js
@@ -0,0 +1,52 @@
+const cityname = document.getElementById('cityname');
+const submitbtn = document.getElementById('submitbtn');
+const city_name = document.getElementById('city_name');
+const temp_real_val = document.getElementById('temp_real_val');
+const temp_status = document.getElementById('temp_status');
+const data_hide = document.querySelector('.middle_layer');
+
+
+const getInfo = async(event) => {
+    event.preventDefault();
+    let cityVal = cityname.value;
+    
+    if(cityVal === ""){
+        city_name.innerText = `Plzz write the name before search`;
+        data_hide.classList.add('data_hide');
+    } else{
+        try{
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=7de418da10f15adcf5182021fd99d835`;
+            const response = await fetch(url);
+            const data = await response.json();
+            const arrData = [data];
+
+            city_name.innerText = `${arrData[0].name},${arrData[0].sys.country}`;
+
+            temp_real_val.innerText = arrData[0].main.temp;
+            
+            // temp_status.innerText = arrData[0].weather[0].main;
+
+            const tempMood = arrData[0].weather[0].main;
+            //condition to check sunny or cloudy 
+            if(tempMood == 'Clear') {
+                temp_status.innerHTML = "<i class='fas fa-sun' style = 'color: #eccc68;'></i>"
+            }else if(tempMood == 'Clouds') {
+                temp_status.innerHTML = "<i class='fas fa-cloud' style = 'color: #f1f2f6;'></i>"
+            }else if(tempMood == 'Rain') {
+                temp_status.innerHTML = "<i class='fas fa-cloud-rain' style = 'color: #a4b0be;'></i>"
+            }else {
+                temp_status.innerHTML = "<i class='fas fa-cloud' style = 'color: #f1f2f6;'></i>"
+            }
+            data_hide.classList.remove('data_hide');
+            
+
+    }catch{
+            city_name.innerText = `Plzz write the city name properly.`;
+            data_hide.classList.add('data_hide');
+        }
+    }
+}
+submitbtn.addEventListener('click', getInfo); 
+
+
+
---
+++ src/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();// iske through express module k properties ko access kr skta hu...
+const path = require('path');
+const hbs = require('hbs');
+const port = 8000;// process.env.PORT || 3000; for hosting
+
+
+//--> path for html file ......public static path
+const staticPath = path.join(__dirname,"../public");
+const templetesPath = path.join(__dirname,"../templetes/views");
+const partialsPath = path.join(__dirname,"../templetes/partials");
+
+// use handlebars(hbs)
+
+app.set('view engine', 'hbs');
+app.set('views', templetesPath);
+hbs.registerPartials(partialsPath);
+app.use(express.static(staticPath));
+
+
+// --------->routinggg <----------
+app.get('/',(req, res) => {
+    res.render('index');
+});
+app.get('/about',(req, res) => {
+    // res.send("Welcome to my first Weather about page");
+    // hbs file lo read krne k liye send k place pe render use krte h..
+    res.render('about');
+});
+app.get('/weather',(req, res) => {
+    res.render("weather");
+});
+app.get('*',(req, res) => {
+    res.render("404error",{
+        errorMsg : "Oops page not found"
+    });
+});// home, about or weather k alawa kuchh search kro to * run kregaa....
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`listening to the port ${port}`)
+});
